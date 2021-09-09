1bcbfe225eee1bd2ecc56a0ded8796dfc2a13e3f
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-09-09 08:33:37 -0300

    model and user controller

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
-import { ForecastController } from './controllers/forecast';
-import { Application } from 'express';
 import * as database from '@src/database';
+import { Application } from 'express';
+import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -23,8 +24,13 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    const beachesController = new BeachesController()
-    this.addControllers([forecastController, beachesController]);
+    const beachesController = new BeachesController();
+    const usersController = new UsersController();
+    this.addControllers([
+      forecastController,
+      beachesController,
+      usersController,
+    ]);
   }
 
   private async databaseSetup(): Promise<void> {
