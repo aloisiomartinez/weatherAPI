0942e41169278550c2fada3a161ff24c72e874fe
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-09-20 16:32:49 -0300

    api-error and fixing tests

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -4,23 +4,23 @@ import { Forecast } from '@src/services/forecast';
 import { Beach } from '@src/models/beach';
 import { authMiddleware } from '@src/middlewares/auth';
 import logger from '@src/logger';
+import { BaseController } from '.';
 
 const forecast = new Forecast();
 
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
-export class ForecastController {
+export class ForecastController extends BaseController{
   @Get('')
   public async getForecastForLoggedUser(req: Request, res: Response): Promise<void> {
     try{
       const beaches = await Beach.find({user: req.decoded?.id});
-
+      
       const forecastData = await forecast.processForecastForBeaches(beaches);
       
       res.status(200).send(forecastData);
     } catch(error: any) {
-      logger.error(error);
-      res.status(500).send({ error: 'Something went wrong' })
+      this.sendErrorResponse(res, { code: 500, message: 'Something went wront'})
     }
   }
 }
